d487259d1e7a8c0ef2b7e803198204cb0e6ce70f
berend.wilkens <berend.wilkens@hu.nl>
2019-09-19 12:07:55 +0200

    index

--- src/main/java/nl/rls/composer/controller/IndexController.java
+++ src/main/java/nl/rls/composer/controller/IndexController.java
@@ -1,10 +1,21 @@
 package nl.rls.composer.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import nl.rls.composer.rest.dto.IndexDto;
+
 @RestController
-@RequestMapping("/api/v1/index")
+@RequestMapping("/")
 public class IndexController {
 
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<IndexDto> getIndex() {
+		IndexDto indexDto = new IndexDto();
+		indexDto.setName("TrainLinkSystem REST api is running.");
+		return ResponseEntity.ok(indexDto);
+	}
 }
