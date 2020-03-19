924f7ceae27f99c29a4bc2b813fff703d754cb4a
joostlek <joostlek@outlook.com>
2020-03-19 16:58:11 +0100

    Refactored CompanyController
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/composer/repository/CompanyRepository.java
+++ src/main/java/nl/rls/composer/repository/CompanyRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface CompanyRepository extends CrudRepository<Company, Integer> {
+    List<Company> findAll();
+
     List<Company> findByCode(String code);
 
     List<Company> findByCountryIso(String countryIso);
