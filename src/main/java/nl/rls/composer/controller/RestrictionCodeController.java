2b7b02a3c374084b6796d277f998748c0b56c2c8
joostlek <joostlek@outlook.com>
2020-03-19 18:41:56 +0100

    Refactor RestrictionCodeController
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/composer/controller/RestrictionCodeController.java
+++ src/main/java/nl/rls/composer/controller/RestrictionCodeController.java
@@ -5,14 +5,16 @@ import nl.rls.composer.domain.code.RestrictionCode;
 import nl.rls.composer.repository.RestrictionCodeRepository;
 import nl.rls.composer.rest.dto.RestrictionCodeDto;
 import nl.rls.composer.rest.dto.mapper.RestrictionCodeDtoMapper;
+import nl.rls.util.Response;
+import nl.rls.util.ResponseBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import javax.persistence.EntityNotFoundException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(BaseURL.BASE_PATH + "/restrictioncodes")
@@ -23,42 +25,35 @@ public class RestrictionCodeController {
         this.restrictionCodeRepository = restrictionCodeRepository;
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<RestrictionCodeDto> getRestrictionCode(@PathVariable Integer id) {
-        Optional<RestrictionCode> optional = restrictionCodeRepository.findById(id);
-        if (optional.isPresent()) {
-            RestrictionCodeDto restrictionCodeDto = RestrictionCodeDtoMapper
-                    .map(optional.get());
-            return ResponseEntity.ok(restrictionCodeDto);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    @GetMapping(value = "/{restrictionCodeId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Response<RestrictionCodeDto> getRestrictionCode(@PathVariable int restrictionCodeId) {
+        RestrictionCode restrictionCode = restrictionCodeRepository.findById(restrictionCodeId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find restriction code with id %d", restrictionCodeId)));
+        RestrictionCodeDto restrictionCodeDto = RestrictionCodeDtoMapper
+                .map(restrictionCode);
+        return ResponseBuilder.ok()
+                .data(restrictionCodeDto)
+                .build();
     }
 
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<RestrictionCodeDto>> getAll(@RequestParam(value = "code", required = false) String code) {
+    @ResponseStatus(HttpStatus.OK)
+    public Response<List<RestrictionCodeDto>> getAll(@RequestParam(value = "code", required = false) String code) {
+        List<RestrictionCode> restrictionCodes;
         if (code == null) {
-            Iterable<RestrictionCode> restrictionCodeList = restrictionCodeRepository.findAll();
-            List<RestrictionCodeDto> restrictionCodeDtoList = new ArrayList<>();
-
-            for (RestrictionCode restrictionCode : restrictionCodeList) {
-                restrictionCodeDtoList.add(RestrictionCodeDtoMapper.map(restrictionCode));
-            }
-            return ResponseEntity.ok(restrictionCodeDtoList);
+            restrictionCodes = restrictionCodeRepository.findAll();
         } else {
-            Optional<RestrictionCode> optional = restrictionCodeRepository.findByCode(code);
-            if (optional.isPresent()) {
-                RestrictionCodeDto restrictionCodeDto = RestrictionCodeDtoMapper
-                        .map(optional.get());
-                return ResponseEntity.ok(Collections.singletonList(restrictionCodeDto));
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-
-//		Link restrictionCodesLink = linkTo(methodOn(RestrictionCodeController.class).getAll()).withSelfRel();
-//		Resources<RestrictionCodeDto> restrictionCodes = new Resources<RestrictionCodeDto>(restrictionCodeDtoList, restrictionCodesLink);
-
+            RestrictionCode restrictionCode = restrictionCodeRepository.findByCode(code)
+                    .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find restriction code %s", code)));
+            restrictionCodes = Collections.singletonList(restrictionCode);
         }
+        List<RestrictionCodeDto> restrictionCodeDtos = restrictionCodes.stream()
+                .map(RestrictionCodeDtoMapper::map)
+                .collect(Collectors.toList());
+        return ResponseBuilder.ok()
+                .data(restrictionCodeDtos)
+                .build();
     }
 }
--- src/main/java/nl/rls/composer/repository/RestrictionCodeRepository.java
+++ src/main/java/nl/rls/composer/repository/RestrictionCodeRepository.java
@@ -4,8 +4,11 @@ package nl.rls.composer.repository;
 import nl.rls.composer.domain.code.RestrictionCode;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RestrictionCodeRepository extends CrudRepository<RestrictionCode, Integer> {
     Optional<RestrictionCode> findByCode(String code);
+
+    List<RestrictionCode> findAll();
 }
