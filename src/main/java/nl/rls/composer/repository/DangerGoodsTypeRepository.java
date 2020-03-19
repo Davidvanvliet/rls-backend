b0920bfe27769b066349e3cbc523795c5849bef2
joostlek <joostlek@outlook.com>
2020-03-19 17:37:02 +0100

    Refactor DangerGoodsTypeController
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/composer/repository/DangerGoodsTypeRepository.java
+++ src/main/java/nl/rls/composer/repository/DangerGoodsTypeRepository.java
@@ -3,5 +3,8 @@ package nl.rls.composer.repository;
 import nl.rls.composer.domain.DangerGoodsType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DangerGoodsTypeRepository extends CrudRepository<DangerGoodsType, Integer> {
+    List<DangerGoodsType> findAll();
 }
