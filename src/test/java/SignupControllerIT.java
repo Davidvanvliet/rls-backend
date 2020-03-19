b181a42caf21cb5bdf82ba648c4b310c4680cff1
JoostLuijben <joost.luijben@student.hu.nl>
2020-03-19 18:21:57 +0100

    Added authentication Controllers

--- src/test/java/SignupControllerIT.java
+++ src/test/java/SignupControllerIT.java
@@ -1,24 +1,21 @@
+import io.restassured.RestAssured;
 import nl.rls.Main;
 import nl.rls.ci.aa.domain.Owner;
-import nl.rls.ci.aa.dto.UserDto;
-import nl.rls.ci.aa.dto.UserPostDto;
 import nl.rls.ci.aa.repository.OwnerRepository;
 import org.flywaydb.test.FlywayTestExecutionListener;
 import org.flywaydb.test.annotation.FlywayTest;
-import org.flywaydb.test.junit5.annotation.FlywayTestExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Main.class)
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
@@ -34,6 +31,7 @@ public class SignupControllerIT {
 
     @BeforeEach
     public void setup() {
+        RestAssured.baseURI = "http://localhost:";
         ownerRepository.save(new Owner("3502", "RaillinkSystems 123"));
     }
 
@@ -48,10 +46,8 @@ public class SignupControllerIT {
                 "  \"password\": \"password\",\n" +
                 "  \"owner\": {\"code\": \"3502\"}\n" +
                 "}";
-        given().body(jsonString).contentType("application/json").post("http://localhost:" + randomServerPort + "/signup")
-                .then().
-                assertThat().statusCode(HttpStatus.OK.value()).body("email", equalTo(email),
-                "firstName", firstName);
+        given().body(jsonString).contentType("application/json").post(RestAssured.baseURI + randomServerPort + "/signup")
+                .then().assertThat().body(matchesJsonSchemaInClasspath("signUp-schema.json"));
 
     }
 }
