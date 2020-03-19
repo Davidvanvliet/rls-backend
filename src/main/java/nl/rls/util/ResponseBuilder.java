ed030ff306a3653bb9c766e00a5f8b2580276698
joostlek <joostlek@outlook.com>
2020-03-19 16:09:52 +0100

    Add more status constructors
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/util/ResponseBuilder.java
+++ src/main/java/nl/rls/util/ResponseBuilder.java
@@ -1,5 +1,7 @@
 package nl.rls.util;
 
+import org.springframework.http.HttpStatus;
+
 public class ResponseBuilder<T> {
     private T data;
     private Object error;
@@ -10,7 +12,39 @@ public class ResponseBuilder<T> {
     }
 
     public static <T> ResponseBuilder<T> ok() {
-        return new ResponseBuilder<>(200);
+        return new ResponseBuilder<>(HttpStatus.OK.value());
+    }
+
+    public static <T> ResponseBuilder<T> created() {
+        return new ResponseBuilder<>(HttpStatus.CREATED.value());
+    }
+
+    public static <T> ResponseBuilder<T> accepted() {
+        return new ResponseBuilder<>(HttpStatus.ACCEPTED.value());
+    }
+
+    public static <T> ResponseBuilder<T> badRequest() {
+        return new ResponseBuilder<>(HttpStatus.BAD_REQUEST.value());
+    }
+
+    public static <T> ResponseBuilder<T> unauthorized() {
+        return new ResponseBuilder<>(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    public static <T> ResponseBuilder<T> forbidden() {
+        return new ResponseBuilder<>(HttpStatus.FORBIDDEN.value());
+    }
+
+    public static <T> ResponseBuilder<T> notFound() {
+        return new ResponseBuilder<>(HttpStatus.NOT_FOUND.value());
+    }
+
+    public static <T> ResponseBuilder<T> status(int status) {
+        return new ResponseBuilder<>(status);
+    }
+
+    public static <T> ResponseBuilder<T> status(HttpStatus status) {
+        return new ResponseBuilder<>(status.value());
     }
 
     public ResponseBuilder<T> data(T data) {
