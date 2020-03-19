d60d9d831ffb6e89624cf5ed80de7299b2eb975a
joostlek <joostlek@outlook.com>
2020-03-19 17:41:13 +0100

    Refactor DangerLabelController
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/composer/controller/DangerLabelController.java
+++ src/main/java/nl/rls/composer/controller/DangerLabelController.java
@@ -6,16 +6,16 @@ import nl.rls.composer.domain.code.DangerLabel;
 import nl.rls.composer.repository.DangerLabelRepository;
 import nl.rls.composer.rest.dto.DangerLabelDto;
 import nl.rls.composer.rest.dto.mapper.DangerLabelDtoMapper;
+import nl.rls.util.Response;
+import nl.rls.util.ResponseBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(BaseURL.BASE_PATH + "/dangerlabels")
@@ -27,28 +27,24 @@ public class DangerLabelController {
         this.dangerLabelRepository = dangerLabelRepository;
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<DangerLabelDto> getById(@PathVariable Integer id) {
-        Optional<DangerLabel> optional = dangerLabelRepository.findById(id);
-        if (optional.isPresent()) {
-            DangerLabelDto dangerLabelDto = DangerLabelDtoMapper.map(optional.get());
-            return ResponseEntity.ok(dangerLabelDto);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    @GetMapping(value = "/{dangerLabelId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Response<DangerLabelDto> getById(@PathVariable int dangerLabelId) {
+        DangerLabel dangerLabel = dangerLabelRepository.findById(dangerLabelId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find danger label with id %d", dangerLabelId)));
+        DangerLabelDto dangerLabelDto = DangerLabelDtoMapper.map(dangerLabel);
+        return ResponseBuilder.ok()
+                .data(dangerLabelDto)
+                .build();
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<DangerLabelDto>> getAll() {
-        Iterable<DangerLabel> entityList = dangerLabelRepository.findAll();
-
-        List<DangerLabelDto> dtoList = new ArrayList<>();
-        for (DangerLabel entity : entityList) {
-            dtoList.add(DangerLabelDtoMapper.map(entity));
-        }
-
-//		Link link = linkTo(methodOn(LocationIdentController.class).getAllQuery("", "")).withSelfRel();
-//		Resources<DangerLabelDto> dtos = new Resources<DangerLabelDto>(dtoList, link);
-        return ResponseEntity.ok(dtoList);
+        List<DangerLabel> dangerLabelList = dangerLabelRepository.findAll();
+        List<DangerLabelDto> dangerLabelDtoList = dangerLabelList.stream()
+                .map(DangerLabelDtoMapper::map)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(dangerLabelDtoList);
     }
 }
--- src/main/java/nl/rls/composer/repository/DangerLabelRepository.java
+++ src/main/java/nl/rls/composer/repository/DangerLabelRepository.java
@@ -3,10 +3,11 @@ package nl.rls.composer.repository;
 import nl.rls.composer.domain.code.DangerLabel;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DangerLabelRepository extends CrudRepository<DangerLabel, Integer> {
     Optional<DangerLabel> findByCode(String code);
 
-
+    List<DangerLabel> findAll();
 }
