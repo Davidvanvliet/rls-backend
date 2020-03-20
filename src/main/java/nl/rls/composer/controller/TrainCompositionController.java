5689f99a56105cce034ce39f264559f487d059de
joostlek <joostlek@outlook.com>
2020-03-20 13:17:25 +0100

    Refactor TrainCompositionController
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/composer/controller/TrainCompositionController.java
+++ src/main/java/nl/rls/composer/controller/TrainCompositionController.java
@@ -7,10 +7,13 @@ import nl.rls.composer.repository.TrainCompositionRepository;
 import nl.rls.composer.rest.dto.TrainCompositionDto;
 import nl.rls.composer.rest.dto.TrainCompositionPostDto;
 import nl.rls.composer.rest.dto.mapper.TrainCompositionDtoMapper;
+import nl.rls.util.Response;
+import nl.rls.util.ResponseBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -26,31 +29,31 @@ public class TrainCompositionController {
         this.securityContext = securityContext;
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TrainCompositionDto> getById(@PathVariable int id) {
+    @GetMapping(value = "/{trainCompositionId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Response<TrainCompositionDto> getById(@PathVariable int trainCompositionId) {
         int ownerId = securityContext.getOwnerId();
-        Optional<TrainComposition> optional = trainCompositionRepository.findByIdAndOwnerId(id, ownerId);
-        if (optional.isPresent()) {
-            TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper.map(optional.get());
-            return ResponseEntity.ok(trainCompositionDto);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        TrainComposition trainComposition = trainCompositionRepository.findByIdAndOwnerId(trainCompositionId, ownerId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find train composition with id %d", trainCompositionId)));
+        TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper.map(trainComposition);
+        return ResponseBuilder.ok()
+                .data(trainCompositionDto)
+                .build();
     }
 
-    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TrainCompositionDto> update(@PathVariable int id, @RequestBody @Valid TrainCompositionPostDto dto) {
+    @PutMapping(value = "/{trainCompositionId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Response<TrainCompositionDto> update(@PathVariable int trainCompositionId,
+                                                @RequestBody @Valid TrainCompositionPostDto dto) {
         int ownerId = securityContext.getOwnerId();
-        Optional<TrainComposition> optional = trainCompositionRepository.findByIdAndOwnerId(id, ownerId);
-        if (optional.isPresent()) {
-            TrainComposition trainComposition = optional.get();
-            trainComposition.setLivestockOrPeopleIndicator(dto.getLivestockOrPeopleIndicator());
-            trainCompositionRepository.save(trainComposition);
-            TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper.map(trainComposition);
-            return ResponseEntity.accepted().body(trainCompositionDto);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        TrainComposition trainComposition = trainCompositionRepository.findByIdAndOwnerId(trainCompositionId, ownerId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find train composition with id %d", trainCompositionId)));
+        trainComposition.setLivestockOrPeopleIndicator(dto.getLivestockOrPeopleIndicator());
+        trainCompositionRepository.save(trainComposition);
+        TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper.map(trainComposition);
+        return ResponseBuilder.accepted()
+                .data(trainCompositionDto)
+                .build();
     }
 
 
