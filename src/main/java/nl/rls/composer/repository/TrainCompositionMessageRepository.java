be59aa77713a9d1315f46c4454ca219413042e63
joostlek <joostlek@outlook.com>
2020-03-23 10:52:48 +0100

    Refactor TrainCompositionMessageController
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/composer/repository/TrainCompositionMessageRepository.java
+++ src/main/java/nl/rls/composer/repository/TrainCompositionMessageRepository.java
@@ -3,11 +3,14 @@ package nl.rls.composer.repository;
 import nl.rls.composer.domain.message.TrainCompositionMessage;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TrainCompositionMessageRepository extends CrudRepository<TrainCompositionMessage, Integer> {
     Optional<TrainCompositionMessage> findByIdAndOwnerId(int id, int ownerId);
 
-    Iterable<TrainCompositionMessage> findByOwnerId(int ownerId);
+    List<TrainCompositionMessage> findByOwnerId(int ownerId);
+
+    boolean existsByIdAndOwnerId(int id, int ownerId);
 
 }
