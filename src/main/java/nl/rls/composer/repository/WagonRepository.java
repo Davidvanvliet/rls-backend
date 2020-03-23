8c231af03c54628b92685cb5c3d0e3af2ec46644
joostlek <joostlek@outlook.com>
2020-03-23 12:05:54 +0100

    Refactor WagonController
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/composer/repository/WagonRepository.java
+++ src/main/java/nl/rls/composer/repository/WagonRepository.java
@@ -3,12 +3,13 @@ package nl.rls.composer.repository;
 import nl.rls.composer.domain.Wagon;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WagonRepository extends CrudRepository<Wagon, Integer> {
-    Iterable<Wagon> findByOwnerId(int ownerId);
+    List<Wagon> findByOwnerId(int ownerId);
 
-    Optional<Wagon> findByOwnerIdAndId(int ownerId, Integer id);
+    Optional<Wagon> findByIdAndOwnerId(int id, int ownerId);
 
-    Optional<Wagon> findByOwnerIdAndNumberFreight(int ownerId, String string);
+    Optional<Wagon> findByNumberFreightAndOwnerId(String numberFreight, int ownerId);
 }
