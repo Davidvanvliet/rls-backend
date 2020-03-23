053fbff642bdcc3c1d1131ab62986a9b2f0881e1
joostlek <joostlek@outlook.com>
2020-03-23 16:56:49 +0100

    Fix merge conflict
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/ci/controller/TcmController.java
+++ src/main/java/nl/rls/ci/controller/TcmController.java
@@ -1,20 +1,5 @@
 package nl.rls.ci.controller;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
-import java.io.IOException;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.xml.sax.SAXException;
-
 import io.swagger.annotations.ApiOperation;
 import nl.rls.ci.aa.security.SecurityContext;
 import nl.rls.ci.domain.CiMessage;
@@ -24,12 +9,17 @@ import nl.rls.composer.domain.message.TrainCompositionMessage;
 import nl.rls.composer.repository.TrainCompositionMessageRepository;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
 import java.util.Optional;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(BaseURL.BASE_PATH + "/tcm")
@@ -48,27 +38,27 @@ public class TcmController {
     @PostMapping(value = "/{id}", produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<String> createTcm(@PathVariable Integer id) throws IOException, SAXException {
         int ownerId = securityContext.getOwnerId();
-		Optional<TrainCompositionMessage> optional = trainCompositionMessageRepository.findByIdAndOwnerId(id, ownerId);
-		if (optional.isPresent()) {
-			TrainCompositionMessage trainCompositionMessage = optional.get();
-			System.out.println(trainCompositionMessage);
-			String tcmMessage = ciService.makeTcmMessage(trainCompositionMessage);
-			CiMessage ciMessage = ciService.makeCiMessage(trainCompositionMessage, tcmMessage);
+        Optional<TrainCompositionMessage> optional = trainCompositionMessageRepository.findByIdAndOwnerId(id, ownerId);
+        if (optional.isPresent()) {
+            TrainCompositionMessage trainCompositionMessage = optional.get();
+            System.out.println(trainCompositionMessage);
+            String tcmMessage = ciService.makeTcmMessage(trainCompositionMessage);
+            CiMessage ciMessage = ciService.makeCiMessage(trainCompositionMessage, tcmMessage);
 
-			System.out.println("POST XML message to databases");
-			return ResponseEntity.created(linkTo(methodOn(CiController.class).getMessage(ciMessage.getId())).toUri())
-					.build();
-		} else {
-			return ResponseEntity.notFound().build();
-		}
-	}
-	
-	/**
-	 * Maakt een nieuw CI bericht aan.
-	 * 
-	 * @param messageXml
-	 * @return de link naar het CI object/resource
-	 */
+            System.out.println("POST XML message to databases");
+            return ResponseEntity.created(linkTo(methodOn(CiController.class).getMessage(ciMessage.getId())).toUri())
+                    .build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Maakt een nieuw CI bericht aan.
+     *
+     * @param messageXml
+     * @return de link naar het CI object/resource
+     */
 //	@Transactional
 //	@PostMapping(value = "/")
 //	@ApiOperation(value = "Stores a CI (XML-)message for a client (UicRequest). This message is not send.")
