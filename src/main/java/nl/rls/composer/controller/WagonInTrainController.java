0688d855b80c447329829be3a2f567e026a4507d
joostlek <joostlek@outlook.com>
2020-03-23 12:26:28 +0100

    Refactor WagonInTrainController
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/composer/controller/WagonInTrainController.java
+++ src/main/java/nl/rls/composer/controller/WagonInTrainController.java
@@ -15,16 +15,15 @@ import nl.rls.composer.rest.dto.WagonInTrainDto;
 import nl.rls.composer.rest.dto.mapper.TrainCompositionDtoMapper;
 import nl.rls.composer.rest.dto.mapper.WagonInTrainDtoMapper;
 import nl.rls.composer.service.TrainCompositionService;
+import nl.rls.util.Response;
+import nl.rls.util.ResponseBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
-import java.util.Optional;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(BaseURL.BASE_PATH + TrainCompositionController.PATH)
@@ -43,102 +42,92 @@ public class WagonInTrainController {
     }
 
     @GetMapping(value = "/{trainCompositionId}/wagons", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<WagonInTrainDto>> getAllWagonInTrain(@PathVariable Integer trainCompositionId) {
+    @ResponseStatus(HttpStatus.OK)
+    public Response<List<WagonInTrainDto>> getAllWagonInTrain(@PathVariable int trainCompositionId) {
         int ownerId = securityContext.getOwnerId();
-        Optional<TrainComposition> optional = trainCompositionRepository
-                .findByIdAndOwnerId(trainCompositionId, ownerId);
-        if (optional.isPresent()) {
-            TrainComposition entity = optional.get();
-            List<WagonInTrainDto> wagonInTrainDtoList = new ArrayList<WagonInTrainDto>();
-            for (WagonInTrain wagonInTrain : entity.getWagons()) {
-                WagonInTrainDto wagonInTrainDto = WagonInTrainDtoMapper.map(wagonInTrain);
-                wagonInTrainDtoList.add(wagonInTrainDto);
-            }
-            return ResponseEntity.ok(wagonInTrainDtoList);
-        }
-        return ResponseEntity.notFound().build();
+        TrainComposition trainComposition = trainCompositionRepository.findByIdAndOwnerId(trainCompositionId, ownerId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find train composition with id %d", trainCompositionId)));
+        List<WagonInTrainDto> wagonInTrainDtos = trainComposition.getWagons().stream()
+                .map(WagonInTrainDtoMapper::map)
+                .collect(Collectors.toList());
+        return ResponseBuilder.ok()
+                .data(wagonInTrainDtos)
+                .build();
     }
 
     @GetMapping(value = "/{trainCompositionId}/wagons/{wagonId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<WagonInTrainDto> getWagonInTrain(@PathVariable Integer trainCompositionId, @PathVariable Integer wagonId) {
+    @ResponseStatus(HttpStatus.OK)
+    public Response<WagonInTrainDto> getWagonInTrain(@PathVariable int trainCompositionId, @PathVariable int wagonId) {
         int ownerId = securityContext.getOwnerId();
-        Optional<TrainComposition> optional = trainCompositionRepository
-                .findByIdAndOwnerId(trainCompositionId, ownerId);
-        if (optional.isPresent()) {
-            TrainComposition entity = optional.get();
-            WagonInTrain wagonInTrain = entity.getWagonById(wagonId);
-            WagonInTrainDto wagonInTrainDto = WagonInTrainDtoMapper.map(wagonInTrain);
-            return ResponseEntity.ok(wagonInTrainDto);
-        }
-        return ResponseEntity.notFound().build();
+        TrainComposition trainComposition = trainCompositionRepository.findByIdAndOwnerId(trainCompositionId, ownerId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find train composition with id %d", trainCompositionId)));
+        WagonInTrain wagonInTrain = trainComposition.getWagonById(wagonId);
+        WagonInTrainDto wagonInTrainDto = WagonInTrainDtoMapper.map(wagonInTrain);
+        return ResponseBuilder.ok()
+                .data(wagonInTrainDto)
+                .build();
     }
 
 
     @PostMapping(value = "/{trainCompositionId}/wagons", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TrainCompositionDto> addWagon(@PathVariable int trainCompositionId,
-                                                        @RequestBody WagonInTrainAddDto dto) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public Response<TrainCompositionDto> addWagon(@PathVariable int trainCompositionId,
+                                                  @RequestBody WagonInTrainAddDto dto) {
         int ownerId = securityContext.getOwnerId();
-        Optional<TrainComposition> optional = trainCompositionRepository
-                .findByIdAndOwnerId(trainCompositionId, ownerId);
-        if (optional.isPresent()) {
-            int wagonId = DecodePath.decodeInteger(dto.getWagonUrl(), "wagons");
-            Optional<Wagon> wagon = wagonRepository.findByOwnerIdAndId(ownerId, wagonId);
-            if (wagon.isPresent()) {
-                TrainComposition trainComposition = optional.get();
 
-                WagonInTrain wagonInTrain = new WagonInTrain();
-                wagonInTrain.setOwnerId(ownerId);
-                wagonInTrain.setWagon(wagon.get());
-                wagonInTrain.setPosition(dto.getPosition());
+        TrainComposition trainComposition = trainCompositionRepository.findByIdAndOwnerId(trainCompositionId, ownerId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find train composition with id %d", trainCompositionId)));
+        int wagonId = DecodePath.decodeInteger(dto.getWagonUrl(), "wagons");
+        Wagon wagon = wagonRepository.findByIdAndOwnerId(wagonId, ownerId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find wagon with id %d", wagonId)));
 
-                wagonInTrain.setBrakeType(BrakeType.G);
-                wagonInTrain.setBrakeWeight(dto.getBrakeWeight());
-                wagonInTrain.setTotalLoadWeight(dto.getTotalLoadWeight());
-                wagonInTrain.setWagonMaxSpeed(dto.getWagonMaxSpeed());
-                trainCompositionService.addWagonToTrain(trainComposition, wagonInTrain);
-                TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper
-                        .map(trainComposition);
-                return ResponseEntity
-                        .created(linkTo(methodOn(TrainCompositionController.class)
-                                .getById(trainComposition.getId())).toUri())
-                        .body(trainCompositionDto);
-            }
-        }
-        return ResponseEntity.notFound().build();
+        WagonInTrain wagonInTrain = new WagonInTrain();
+        wagonInTrain.setOwnerId(ownerId);
+        wagonInTrain.setWagon(wagon);
+        wagonInTrain.setPosition(dto.getPosition());
+
+        wagonInTrain.setBrakeType(BrakeType.G);
+        wagonInTrain.setBrakeWeight(dto.getBrakeWeight());
+        wagonInTrain.setTotalLoadWeight(dto.getTotalLoadWeight());
+        wagonInTrain.setWagonMaxSpeed(dto.getWagonMaxSpeed());
+        trainCompositionService.addWagonToTrain(trainComposition, wagonInTrain);
+        TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper
+                .map(trainComposition);
+        return ResponseBuilder.created()
+                .data(trainCompositionDto)
+                .build();
     }
 
     @PutMapping(value = "/{trainCompositionId}/wagons/{wagonInTrainId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TrainCompositionDto> moveWagon(@PathVariable int trainCompositionId,
-                                                         @PathVariable int wagonInTrainId, @RequestParam("position") int position) {
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Response<TrainCompositionDto> moveWagon(@PathVariable int trainCompositionId,
+                                                   @PathVariable int wagonInTrainId,
+                                                   @RequestParam("position") int position) {
         int ownerId = securityContext.getOwnerId();
-        Optional<TrainComposition> optional = trainCompositionRepository
-                .findByIdAndOwnerId(trainCompositionId, ownerId);
-        if (optional.isPresent()) {
-            TrainComposition trainComposition = optional.get();
-            trainCompositionService.moveWagonById(trainComposition, wagonInTrainId, position);
-            TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper
-                    .map(trainComposition);
-            return ResponseEntity.accepted().body(trainCompositionDto);
-        }
-        return ResponseEntity.notFound().build();
+        TrainComposition trainComposition = trainCompositionRepository.findByIdAndOwnerId(trainCompositionId, ownerId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find train composition with id %d", trainCompositionId)));
+        trainCompositionService.moveWagonById(trainComposition, wagonInTrainId, position);
+        TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper
+                .map(trainComposition);
+        return ResponseBuilder.accepted()
+                .data(trainCompositionDto)
+                .build();
     }
 
-    @DeleteMapping(value = "/{id}/wagons/{wagonInTrainId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TrainCompositionDto> removeWagon(@PathVariable int id,
-                                                           @PathVariable int wagonInTrainId) {
+    @DeleteMapping(value = "/{trainCompositionId}/wagons/{wagonInTrainId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Response<TrainCompositionDto> removeWagon(@PathVariable int trainCompositionId,
+                                                     @PathVariable int wagonInTrainId) {
         int ownerId = securityContext.getOwnerId();
-        Optional<TrainComposition> optional = trainCompositionRepository
-                .findByIdAndOwnerId(id, ownerId);
-        if (optional.isPresent()) {
-            TrainComposition trainComposition = optional.get();
-            trainComposition.removeWagonById(wagonInTrainId);
-//			wagonInTrainRepository.saveAll(trainCompositionJourneySection.getWagons());
-            trainCompositionRepository.save(trainComposition);
-            TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper
-                    .map(trainComposition);
-            return ResponseEntity.ok(trainCompositionDto);
-        }
-        return ResponseEntity.notFound().build();
+        TrainComposition trainComposition = trainCompositionRepository.findByIdAndOwnerId(trainCompositionId, ownerId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Could not find train composition with id %d", trainCompositionId)));
+        trainComposition.removeWagonById(wagonInTrainId);
+        trainCompositionRepository.save(trainComposition);
+        TrainCompositionDto trainCompositionDto = TrainCompositionDtoMapper
+                .map(trainComposition);
+        return ResponseBuilder.ok()
+                .data(trainCompositionDto)
+                .build();
     }
 
 }
