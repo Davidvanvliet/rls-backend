31f40c2267c5bbb451a1bef444d2513bb4a699bf
joostlek <joostlek@outlook.com>
2020-05-20 13:22:45 +0200

    Replace equals and hashcode with annotation
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/rls/composer/domain/TractionInTrain.java
+++ src/main/java/nl/rls/composer/domain/TractionInTrain.java
@@ -1,5 +1,6 @@
 package nl.rls.composer.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 public class TractionInTrain {
     /**
      * 0 - no driver present in Loco, 1 - driver(s) is /are) present in Loco
@@ -36,30 +38,5 @@ public class TractionInTrain {
     private Traction traction;
     @ManyToOne
     private TrainComposition trainComposition;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        TractionInTrain other = (TractionInTrain) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
 
 }
