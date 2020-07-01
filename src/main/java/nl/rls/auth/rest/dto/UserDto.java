3860e17b99621f946e93c3e89bfc2dc5394de807
joostlek <joostlek@outlook.com>
2020-07-01 11:22:39 +0200

    Add getters and setters

--- src/main/java/nl/rls/auth/rest/dto/UserDto.java
+++ src/main/java/nl/rls/auth/rest/dto/UserDto.java
@@ -1,5 +1,12 @@
 package nl.rls.auth.rest.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserDto {
     private String auth0Id;
 }
